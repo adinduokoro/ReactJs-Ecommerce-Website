af053aad7b31a84872a3eb2ceb14d6f06d5e346a
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-05-01 20:01:17 -0400

    Initial Code
    Initial project setup. Pages and components fixed. Navbar and header created. Working on redux store.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+//pages
+import {Home, CategoryProduct, ProductSingle, Cart, Search} from "./pages/index";
+//components
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
+//store
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Sidebar />
+      </BrowserRouter>
     </div>
   );
 }
